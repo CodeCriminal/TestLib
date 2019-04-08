fb84fc43083095c7d3d7987799577de4b0177d16
CodeCriminal <38607570+CodeCriminal@users.noreply.github.com>
2019-04-08 18:51:08 +0300

    Add files via upload

---
+++ src/main/java/android/savage/testlibrary/MainActivity.java
@@ -0,0 +1,15 @@
+package android.savage.testlibrary;
+
+import android.app.Activity;
+
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
